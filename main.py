40a1fecb7385e8c17e28b5dced2bc7283a24869e
Tom Ford <tomfordweb@gmail.com>
2020-09-18 19:22:07 -0400

    rolling dice is coller than flipping coins anyways

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import click
+import random
 
 @click.command()
-def headsOrTails():
-    click.echo('hello world')
+@click.option('--count', prompt='How many rolls')
+@click.option('--sides', prompt='How many sides')
+def command(count, sides):
+    click.echo('Rolling a D%s %s time(s)' % (count, sides))
+    sides = int(sides)
+    for i in range(1, int(count)):
+        rollDice(i, sides)
 
+
+
+def rollDice(iterator, sides):
+    result = random.randint(1, sides)
+    result = str(result)
+    click.echo('Roll %s: %s' % (iterator, result))
 
 if __name__ == '__main__':
-    headsOrTails()
+    command()
